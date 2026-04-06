d408b23f9e2158a1329d1ad6d7d5b3d508d540a9
Mohit Godara <mohitgodara@Mohits-MacBook-Air-3.local>
2026-04-06 22:44:35 +0530

    work on a Dashboard page

--- Server/src/app.js
+++ Server/src/app.js
@@ -6,7 +6,7 @@ import authRoutes from "./routes/auth.routes.js";
 const app = express();
 
 
-app.use(cors({ origin: "http://localhost:5173", "", credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://leetcore-1.onrender.com"], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
